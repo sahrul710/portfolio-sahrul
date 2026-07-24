54a4391a9d6c49997568046e9251d5566617ddef
sahrul710 <sahrulsahrul710@gmail.com>
2026-07-24 08:27:06 +0700

    update contact section

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,44 @@ window.onscroll = () => {
     scrollup.classList.remove("scroll-active");
   }
 };
+
+// Tombol Geser Sertifikat
+
+const sertifikatBox = document.querySelector(".sertifikat-box");
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.addEventListener("click", () => {
+  sertifikatBox.scrollBy({
+    left: 320,
+    behavior: "smooth",
+  });
+});
+
+emailjs.init("0WmBNWbSwkPmrU1ku");
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs
+    .sendForm(
+      "service_jmdoi0g",
+
+      "template_5e0wohe",
+
+      this,
+    )
+
+    .then(() => {
+      alert("Pesan berhasil dikirim!");
+
+      form.reset();
+    })
+
+    .catch((error) => {
+      alert("Pesan gagal dikirim!");
+
+      console.log(error);
+    });
+});
